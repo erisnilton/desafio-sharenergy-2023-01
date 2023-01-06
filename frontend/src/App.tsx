de36b6bcf09e83048cbb4b49edb163423b937213
Erisnilton Freitas <erisniltongf@gmail.com>
2023-01-06 12:09:04 -0300

    chore: modify file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import "./App.css";
+import Navbar from "./components/navbar";
+import theme from "./theme";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RandomUser from "./pages/random-user";
+import HttpCat from "./pages/http-cat";
+import RandomDog from "./pages/random-dog";
+import Customer from "./pages/customer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Hello</h1>
-  )
+    <BrowserRouter basename="/">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<RandomUser />} />
+          <Route path="/http-cat" element={<HttpCat />} />
+          <Route path="/random-dog" element={<RandomDog />} />
+          <Route path="/customer" element={<Customer />} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
