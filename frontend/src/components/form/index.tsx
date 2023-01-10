919f61e81afa2a2439bda0b907193932f3b2f1d0
Erisnilton Freitas <erisniltongf@gmail.com>
2023-01-10 11:13:15 -0300

    feat: add component form

---
+++ frontend/src/components/form/index.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiFormControl-root": {
+      width: "100%",
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export function Form(props: any) {
+  const classes = useStyles();
+  const { children, ...other } = props;
+  return (
+    <form className={classes.root} autoComplete="off" {...other}>
+      {props.children}
+    </form>
+  );
+}
